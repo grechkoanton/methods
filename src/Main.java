63e7290f7ebeb0188355388eaf83a257964a1ac9
HP <grechkoantonkrd@gmail.com>
2025-05-13 02:59:10 +0300

    HW Done Repeat

--- src/Main.java
+++ src/Main.java
@@ -4,57 +4,57 @@ public class Main {
     public static void task1() {
         System.out.println("Task 1");
     }
-    public static void checkLeapYear(int a, int b) {
-        if (a <= b) {
-            System.out.println(a + " год не является високосным.");
-        } else if (a % 400 == 0) {
-            System.out.println(a + " год является високосным.");
-        } else if (a % 100 == 0) {
-            System.out.println(a + " год не является високосным.");
-        } else if (a % 4 == 0) {
-            System.out.println(a + " год является високосным.");
+    public static void checkYear(int variableYear) {
+        if (variableYear % 400 == 0) {
+            System.out.println(variableYear + " год является високосным.");
+        } else if (variableYear % 100 == 0) {
+            System.out.println(variableYear + " год не является високосным.");
+        } else if (variableYear % 4 == 0) {
+            System.out.println(variableYear + " год является високосным.");
         } else {
-            System.out.println(a + " год не является високосным.");
+            System.out.println(variableYear + " год не является високосным.");
         }
     }
 
     public static void task2() {
         System.out.println("Task 2");
     }
-    public static void checkOS(int c, int d) {
-        if (c == 0 && d < 2015) {
+    public static void checkOS(int operatingSystem, int year) {
+        if (operatingSystem == 0 && year < 2015) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        } else if (c == 0 && d >= 2015) {
+        } else if (operatingSystem == 0 && year >= 2015) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        }
-        if (c == 1 && d < 2015) {
+        } else if (operatingSystem == 1 && year < 2015) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        } else if (c == 1 && d >= 2015) {
+        } else if (operatingSystem == 1 && year >= 2015) {
             System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Такая версия приложения не предусмотрена в программе");
         }
     }
 
     public static void task3() {
         System.out.println("Task 3");
     }
-    public static int homeDeliveryCard(int f, int g) {
-        if (f <= 20) {
-            System.out.println("Потребуется дней на доставку: " + g + " день.");
-        } else if (f > 20 && f <= 60) {
-            System.out.println("Потребуется дней на доставку: " + (g + 1) + " дня.");
-        } else if (f > 60 && f <= 100) {
-            System.out.println("Потребуется дней на доставку: " + (g + 1 + 1) + " дня.");
+
+    public static int checkTheDistanceFromTheOfficeToDeliveryAddress(int distance) {
+        int daysDeliveryInterval = 1;
+        if (distance < 20) {
+            System.out.println("Потребуется дней на доставку: " + daysDeliveryInterval + " день.");
+        } else if (distance >= 20 && distance < 60) {
+            System.out.println("Потребуется дней на доставку: " + (daysDeliveryInterval + 1) + " дня.");
+        } else if (distance >= 60 && distance < 100) {
+            System.out.println("Потребуется дней на доставку: " + (daysDeliveryInterval + 1 + 1) + " дня.");
         } else {
             System.out.println("Свыше 100 км доставки нет.");
         }
-        return (g);
+        return (daysDeliveryInterval);
     }
 
     public static void main(String[] args) {
         task1();
-        int year = 2025;
-        int leapYearIntroduced = 1584;
-        checkLeapYear(year, leapYearIntroduced);
+        int year = 2024;
+        checkYear(year);
 
         task2();
         int clientOS = 1;
@@ -62,8 +62,8 @@ public class Main {
         checkOS(clientOS, currentYear);
 
         task3();
-        int deliveryDistance = 95;
-        int dayIn20Km = 1;
-        homeDeliveryCard(deliveryDistance, dayIn20Km);
+        int deliveryDistance = 30;
+        checkTheDistanceFromTheOfficeToDeliveryAddress(deliveryDistance);
     }
 }
+
