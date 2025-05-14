6464262ec11fc15fd72af48dae15bdd142914456
HP <grechkoantonkrd@gmail.com>
2025-05-14 04:03:06 +0300

    HW Done Repeat 2

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,37 @@
 import java.time.LocalDate;
 
 public class Main {
-    public static void task1() {
-        System.out.println("Task 1");
+    public static void main(String[] args) {
+        printTask(1);
+        int year = 2025;
+        checkYear(year);
+
+        printTask(2);
+        int clientOS = 1;
+        int currentYear = LocalDate.now().getYear(); //int clientDeviceYear = 2025;
+        checkOS(clientOS, currentYear);
+
+        printTask(3);
+        int deliveryDistance = 20;
+        checkTheDistanceFromTheOfficeToDeliveryAddress(deliveryDistance);
+        int days = checkTheDistanceFromTheOfficeToDeliveryAddress(deliveryDistance);
+        System.out.println("Потребуется дней на доставку: " + days);
     }
+
+    public static void printTask(int number) {
+        System.out.println("Task " + number);
+    }
+
+    // Task 1
     public static void checkYear(int variableYear) {
-        if (variableYear % 400 == 0) {
-            System.out.println(variableYear + " год является високосным.");
-        } else if (variableYear % 100 == 0) {
-            System.out.println(variableYear + " год не является високосным.");
-        } else if (variableYear % 4 == 0) {
+        if (variableYear % 400 == 0 || variableYear % 4 == 0 && variableYear % 100 != 0) {
             System.out.println(variableYear + " год является високосным.");
         } else {
             System.out.println(variableYear + " год не является високосным.");
         }
     }
 
-    public static void task2() {
-        System.out.println("Task 2");
-    }
+    //Task 2
     public static void checkOS(int operatingSystem, int year) {
         if (operatingSystem == 0 && year < 2015) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
@@ -33,37 +46,20 @@ public class Main {
         }
     }
 
-    public static void task3() {
-        System.out.println("Task 3");
-    }
-
+    // Task 3
     public static int checkTheDistanceFromTheOfficeToDeliveryAddress(int distance) {
-        int daysDeliveryInterval = 1;
+        int daysDeliveryInterval1 = 0; //нужна ли тут переменная? или лучше без нее
         if (distance < 20) {
-            System.out.println("Потребуется дней на доставку: " + daysDeliveryInterval + " день.");
+            return daysDeliveryInterval1 + 1; //return 1
         } else if (distance >= 20 && distance < 60) {
-            System.out.println("Потребуется дней на доставку: " + (daysDeliveryInterval + 1) + " дня.");
+            return 2; //daysDeliveryInterval1 + 2;
         } else if (distance >= 60 && distance < 100) {
-            System.out.println("Потребуется дней на доставку: " + (daysDeliveryInterval + 1 + 1) + " дня.");
+            return 3; //daysDeliveryInterval1 + 3;
         } else {
-            System.out.println("Свыше 100 км доставки нет.");
+            return 0;
         }
-        return (daysDeliveryInterval);
-    }
 
-    public static void main(String[] args) {
-        task1();
-        int year = 2024;
-        checkYear(year);
 
-        task2();
-        int clientOS = 1;
-        int currentYear = LocalDate.now().getYear(); //int clientDeviceYear = 2025;
-        checkOS(clientOS, currentYear);
-
-        task3();
-        int deliveryDistance = 30;
-        checkTheDistanceFromTheOfficeToDeliveryAddress(deliveryDistance);
     }
 }
 
